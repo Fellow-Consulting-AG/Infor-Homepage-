34b4f9cbefecb4382deffe06cc3223a0e7463b20
Farooq Ahmed Khan <farooqk.work@gmail.com>
2021-03-20 00:48:29 +0500

    2nd iteration of this widget completed

--- Widgets/de.fellow.PDF/main.ts
+++ Widgets/de.fellow.PDF/main.ts
@@ -21,14 +21,20 @@ interface ListItem {
     entrance: string
 }
 
+interface VerticalListItem {
+    date: string;
+    rollover: boolean;
+}
+
 @Component({
     template: `
         <div class="parent-layout">
             <div>
                 <!--  Top Button Row   -->
                 <div class="icon-row">
-                    <div *ngFor="let menuItem of topMenuItems;">
-                        <div style="display: grid; grid-template-rows: 1fr minmax(25px, auto);" class="main-menu-icon">
+                    <div *ngFor="let menuItem of topMenuItems;let i = index;">
+                        <div style="display: grid; grid-template-rows: 1fr minmax(25px, auto);" class="main-menu-icon"
+                             (click)="currentTab = i;">
                             <div style="text-align: center"><img [src]="menuItem.icon" class="main-menu-icon-image"/>
                             </div>
                             <div style="text-align: center">{{menuItem.label}}</div>
@@ -36,94 +42,138 @@ interface ListItem {
                     </div>
                 </div>
             </div>
-            <div class="body-grid">
 
-                <!-- List    -->
-                <div class="list-container">
-                    <div *ngFor="let item of sampleListItems;" class="list-item">
-                        <div style="padding-left: 10px;">
-                            <img [src]="checkMark" width="20" *ngIf="item.checked">
-                            <div style="width:20px" *ngIf="!item.checked"></div>
-                        </div>
-                        <div>{{item.date}}</div>
-                        <div>{{item.sender}}</div>
-                        <div>{{item.cashRegister}}</div>
-                        <div>{{item.firstName}}</div>
-                        <div>{{item.lastName}}</div>
-                        <div>{{item.entrance}}</div>
-                    </div>
-                </div>
-
-                <!-- PDF Preview     -->
-                <div style="grid-column: 2; grid-row: 1/-1; padding:10px 10px 20px 10px;">
-                    <iframe src="http://www.muhammadbinyusrat.com/devguide.pdf" width="100%" height="100%"
-                            style="box-shadow: 0 10px 6px -6px #777;"></iframe>
-                </div>
-
-                <!-- Form  -->
-                <div class="form-box">
-                    <div class="form-container">
-                        <div class="form-fields-container">
-                            <div class="field">
-                                <label>User</label>
-                                <input type="text" class="smaller-text-field search-icon" value="4401ac15">
+            <!-- First Tab -->
+            <ng-container *ngIf="currentTab == 0">
+                <div class="body-grid two-by-two">
+                    <!-- List    -->
+                    <div class="list-container">
+                        <div *ngFor="let item of sampleListItems;" class="list-item">
+                            <div style="padding-left: 10px;">
+                                <img [src]="checkMark" width="20" *ngIf="item.checked">
+                                <div style="width:20px" *ngIf="!item.checked"></div>
                             </div>
-                            <div class="field">
-                                <label></label>
-                                <span style="font-size:14px">Obermeier, Klaus</span>
-                            </div>
-
-                            <div class="field">
-                                <label>Kassenzeichen</label>
-                                <input type="text" class="smaller-text-field" value="3202039423042">
-                            </div>
-                            <div>&nbsp;</div>
-
-
-                            <div class="field">
-                                <label>Nachname</label>
-                                <input type="text" class="smaller-text-field" value="Panse">
-                            </div>
-                            <div class="field">
-                                <label>Vorname</label>
-                                <input type="text" class="smaller-text-field" value="Jim">
-                            </div>
-
-
-                            <div class="field" style="grid-row:4; grid-column: 1 / -1">
-                                <label>Eingang</label>
-                                <input type="text" class="smaller-text-field" value="Posteinganj -- Ruckleuf EMA"
-                                       style="width:90%">
-                            </div>
-
-
-                            <div class="field" style="grid-row:5; grid-column: 1 / -1">
-                                <label>Dokumentenbezeichnung</label>
-                                <input type="text" class="smaller-text-field search-icon" value="Ruckleuf EMA"
-                                       style="width:90%">
-                            </div>
-                        </div>
-                        <div class="push-to-bottom">
-                            <div></div>
-                            <div>
-                                <button class="outlined-button">schließen</button>
-                            </div>
-                        </div>
-                        <div class="push-to-bottom">
-                            <div></div>
-                            <div>
-                                <button class="filled-button">eAkte</button>
-                            </div>
-                            <div>
-                                <button class="filled-button">Dokument</button>
-                            </div>
-                            <div>
-                                <button class="filled-button">Speichern</button>
-                            </div>
+                            <div>{{item.date}}</div>
+                            <div>{{item.sender}}</div>
+                            <div>{{item.cashRegister}}</div>
+                            <div>{{item.firstName}}</div>
+                            <div>{{item.lastName}}</div>
+                            <div>{{item.entrance}}</div>
                         </div>
                     </div>
+
+                    <!-- PDF Preview     -->
+                    <div style="grid-column: 2; grid-row: 1/-1; padding:10px 10px 20px 10px;">
+                        <iframe src="http://www.muhammadbinyusrat.com/devguide.pdf" width="100%" height="100%"
+                                class="pdf-container-style"></iframe>
+                    </div>
+
+                    <!-- Form  -->
+                    <div class="form-box">
+                        <div class="form-container">
+                            <div class="form-fields-container">
+                                <div class="field">
+                                    <label>User</label>
+                                    <input type="text" class="smaller-text-field search-icon" value="4401ac15">
+                                </div>
+                                <div class="field">
+                                    <label></label>
+                                    <span style="font-size:14px">Obermeier, Klaus</span>
+                                </div>
+
+                                <div class="field">
+                                    <label>Kassenzeichen</label>
+                                    <input type="text" class="smaller-text-field" value="3202039423042">
+                                </div>
+                                <div>&nbsp;</div>
+
+
+                                <div class="field">
+                                    <label>Nachname</label>
+                                    <input type="text" class="smaller-text-field" value="Panse">
+                                </div>
+                                <div class="field">
+                                    <label>Vorname</label>
+                                    <input type="text" class="smaller-text-field" value="Jim">
+                                </div>
+
+
+                                <div class="field" style="grid-row:4; grid-column: 1 / -1">
+                                    <label>Eingang</label>
+                                    <input type="text" class="smaller-text-field" value="Posteinganj -- Ruckleuf EMA"
+                                           style="width:90%">
+                                </div>
+
+
+                                <div class="field" style="grid-row:5; grid-column: 1 / -1">
+                                    <label>Dokumentenbezeichnung</label>
+                                    <input type="text" class="smaller-text-field search-icon" value="Ruckleuf EMA"
+                                           style="width:90%">
+                                </div>
+                            </div>
+                            <div class="push-to-bottom">
+                                <div></div>
+                                <div>
+                                    <button class="outlined-button">schließen</button>
+                                </div>
+                            </div>
+                            <div class="push-to-bottom">
+                                <div></div>
+                                <div>
+                                    <button class="filled-button">eAkte</button>
+                                </div>
+                                <div>
+                                    <button class="filled-button">Dokument</button>
+                                </div>
+                                <div>
+                                    <button class="filled-button">Speichern</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </ng-container>
+
+            <ng-container *ngIf="currentTab == 1">
+                <div class="body-grid two-by-one">
+                    <div class="vertical-items-container">
+                        <div class="vertical-items">
+
+                            <div *ngFor="let verticalItem of verticalItems;" class="vertical-item"
+                                 [ngClass]="{'hover-shadow':verticalItem.rollover}"
+                                 (mouseover)="verticalItem.rollover = true" (mouseout)="verticalItem.rollover= false">
+                                <div>
+                                    12.12.2020
+                                </div>
+                                <div></div>
+                                <div class="vertical-item-details">
+                                    <div style="grid-row: 1/-1;margin-left: 10px; margin-top: 5px">
+                                        <img [src]="checkMark" width="30">
+                                    </div>
+                                    <div style="margin-top: 10px; grid-column: 2/4">Poststelle</div>
+                                    <div style="margin-top: 10px;">3206002</div>
+                                    <div style="grid-column: 5; grid-row: 1/span 2;margin-top: 5px;"><img [src]="pdfIcon"></div>
+
+                                    <div>Reich</div>
+                                    <div>Frank</div>
+                                    <div>Unbekannt</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="vertical-line">
+                            <div></div>
+                            <div style="background-color: #6a98b4;"></div>
+                        </div>
+
+                    </div>
+                    <!-- PDF Preview     -->
+                    <div style="padding:10px 10px 20px 10px;">
+                        <iframe src="http://www.muhammadbinyusrat.com/devguide.pdf" width="100%" height="100%"
+                                class="pdf-container-style"></iframe>
+                    </div>
+                </div>
+            </ng-container>
         </div>
     `,
     styles: [`
@@ -159,8 +209,20 @@ interface ListItem {
             background-color: #f0f0f0;
             display: grid;
             gap: 2rem;
+        }
+
+        .two-by-two {
             grid-template-columns: 1fr 1fr;
             grid-template-rows: 1fr 1fr;
+        }
+
+        .two-by-one {
+            grid-template-columns: 1fr 1fr;
+            grid-template-rows: 1fr;
+        }
+
+        .pdf-container-style {
+            box-shadow: 0 10px 6px -6px #777;
         }
 
         .list-container {
@@ -256,6 +318,83 @@ interface ListItem {
         .search-icon {
             background: url("data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0iYmxhY2siIHdpZHRoPSIxOHB4IiBoZWlnaHQ9IjE4cHgiPjxwYXRoIGQ9Ik0wIDBoMjR2MjRIMHoiIGZpbGw9Im5vbmUiLz48cGF0aCBkPSJNMTUuNSAxNGgtLjc5bC0uMjgtLjI3QzE1LjQxIDEyLjU5IDE2IDExLjExIDE2IDkuNSAxNiA1LjkxIDEzLjA5IDMgOS41IDNTMyA1LjkxIDMgOS41IDUuOTEgMTYgOS41IDE2YzEuNjEgMCAzLjA5LS41OSA0LjIzLTEuNTdsLjI3LjI4di43OWw1IDQuOTlMMjAuNDkgMTlsLTQuOTktNXptLTYgMEM3LjAxIDE0IDUgMTEuOTkgNSA5LjVTNy4wMSA1IDkuNSA1IDE0IDcuMDEgMTQgOS41IDExLjk5IDE0IDkuNSAxNHoiLz48L3N2Zz4=") no-repeat right;
         }
+
+
+        /* **************************** */
+        /* Second Page (Vertical Items) */
+        /* **************************** */
+
+        .vertical-items-container {
+            position: relative;
+            width: 100%;
+            margin: 20px 0px;
+        }
+
+        .vertical-line {
+            display: grid;
+            grid-template-columns: 1fr 10px;
+            grid-template-rows: 1fr;
+            position: absolute;
+            top: 0;
+            left: 0;
+            height: 100%;
+            width: 200px;
+        }
+
+        .vertical-items {
+            position: absolute;
+            top: 0;
+            left: 0;
+            display: grid;
+            gap: 3rem;
+            grid-template-rows: repeat(auto-fill, 1fr);
+            width: 100%;
+            padding-top: 25px;
+        }
+
+        .vertical-item {
+            cursor: pointer;
+            display: grid;
+            grid-template-columns: 150px 50px 1fr;
+            gap: 2rem;
+            z-index: 5;
+        }
+
+        .hover-shadow > * {
+            box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.034),
+            0 6.7px 5.3px rgba(0, 0, 0, 0.048),
+            0 12.5px 10px rgba(0, 0, 0, 0.06),
+            0 22.3px 17.9px rgba(0, 0, 0, 0.072),
+            0 41.8px 33.4px rgba(0, 0, 0, 0.086),
+            0 100px 80px rgba(0, 0, 0, 0.12)
+        }
+
+        .vertical-item > div:nth-child(1) {
+            background-color: white;
+            height: 10px;
+            place-self: center;
+            padding: 10px 15px 25px 15px;
+            font-size: 14px;
+        }
+
+        .vertical-item > div:nth-child(2) { /* Middle Circle */
+            background-color: #2678a9;
+            height: 50px;
+            border-radius: 50%;
+        }
+
+        .vertical-item > div:nth-child(3) {
+            background-color: white;
+            place-self: center;
+        }
+
+        .vertical-item-details {
+            font-size: 14px;
+            display: grid;
+            grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+            grid-template-rows: 1fr 1fr;
+            gap: 3rem;
+        }
     `]
     // changeDetection: ChangeDetectionStrategy.OnPush
 })
@@ -266,9 +405,12 @@ export class PDFComponent implements OnInit, IWidgetComponent {
     jQueryElement?: JQuery; //Reference to JQuery
     $ = $;
     checkMark: SafeHtml;
+    pdfIcon: SafeHtml;
 
     topMenuItems: MainMenuItem[] = [];
     sampleListItems: ListItem[];
+    currentTab: 0 | 1 | 2 | 3 = 1;
+    verticalItems: VerticalListItem[];
 
     constructor(private readonly changeDetectionRef: ChangeDetectorRef, private fb: FormBuilder, private ds: DomSanitizer) {
     }
@@ -287,6 +429,7 @@ export class PDFComponent implements OnInit, IWidgetComponent {
         ];
 
         this.checkMark = this.ds.bypassSecurityTrustUrl(assets.check);
+        this.pdfIcon = this.ds.bypassSecurityTrustUrl(assets.pdfIcon);
 
         this.sampleListItems = [
             {
@@ -346,6 +489,14 @@ export class PDFComponent implements OnInit, IWidgetComponent {
 
         ];
 
+
+        this.verticalItems = [
+            {date: '12.12.2020', rollover: false},
+            {date: '26.10.2020', rollover: false},
+            {date: '12.12.2020', rollover: false}
+        ];
+
+
         try {
             $('body').initialize('en-US');
             $('#searchfield').searchfield({
@@ -368,6 +519,10 @@ export class PDFComponent implements OnInit, IWidgetComponent {
             type: WidgetSettingsType.selectorType,
             name: "order",
         }];
+    }
+
+    switchTab(i: number) {
+        alert(i);
     }
 }
 
