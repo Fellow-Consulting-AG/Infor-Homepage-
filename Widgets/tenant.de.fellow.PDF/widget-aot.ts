9f72159c0d87b81c015e4e48971707bff35d93cc
Farooq Ahmed Khan <farooqk.work@gmail.com>
2021-03-25 16:30:49 +0500

    Create a tenant widget copy of an existing widget

---
+++ Widgets/tenant.de.fellow.PDF/widget-aot.ts
@@ -0,0 +1,12 @@
+﻿import { IWidgetContext, IWidgetInstance } from "lime";
+import { PDFComponent} from "./main";
+import { PdfModuleNgFactory } from "./main.ngfactory";
+
+export const widgetFactory = (context: IWidgetContext): IWidgetInstance => {
+	return {
+		angularConfig: {
+			moduleFactory: PdfModuleNgFactory,
+			componentType: PDFComponent
+		}
+	};
+};
